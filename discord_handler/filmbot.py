52fd94dc507af0b5e62c84702f2ebb2bc0d5ead2
Elliot Goodrich <elliotgoodrich@gmail.com>
2022-01-04 17:28:39 +0000

    Add nominate_film function
    Add functionality for a user to nominate a film.  Add a `UserError`
    exception class for all error messages that should be shown to the user.

--- discord_handler/filmbot.py
+++ discord_handler/filmbot.py
@@ -1,4 +1,5 @@
 import boto3
+from UserError import UserError
 
 MEMBERS_TABLE = "filmbot-members"
 # filmbot-members
@@ -55,10 +56,11 @@ VOTES_TABLE = "filmbot-votes"
 
 
 class FilmBot:
-    def __init__(self, dynamodb):
+    def __init__(self, dynamodb, dynamodb_client):
         self._members_table = dynamodb.Table(MEMBERS_TABLE)
         self._nominations_table = dynamodb.Table(NOMINATIONS_TABLE)
         self._vote_table = dynamodb.Table(VOTES_TABLE)
+        self._dynamodb_client = dynamodb_client
 
     @property
     def members_table(self):
@@ -72,23 +74,77 @@ class FilmBot:
     def votes_table(self):
         return self._votes_table
 
+    @property
+    def client(self):
+        return self._dynamodb_client
+
     def register_user(self, discord_user_id, datetime):
         """Attempt to register the specified `discord_user_id` at the specified `datetime`.
         If `discord_user_id` is already registered, then throw an exception."""
-        self.members_table.put_item(
-            Item={
-                "discord-user-id": discord_user_id,
-                "date-joined": datetime.isoformat(),
-                "nominated-film-id": None,
-                "vote-id": None,
-                "attendance-vote-id": None,
-            },
-            ConditionExpression="attribute_not_exists(discord-user-id)",
-        )
+        try:
+            self.members_table.put_item(
+                Item={
+                    "discord-user-id": discord_user_id,
+                    "date-joined": datetime.isoformat(),
+                    "nominated-film-id": None,
+                    "vote-id": None,
+                    "attendance-vote-id": None,
+                },
+                ConditionExpression="attribute_not_exists(discord-user-id)",
+            )
+        except self.client.exceptions.ConditionalCheckFailedException as e:
+            raise UserError("User is already registered")
 
-    def nominate_film(self, discord_user_id, film_name):
-        # TODO
-        assert False
+    def nominate_film(self, *, DiscordUserID, FilmName, NewFilmID, DateTime):
+        """Attempt to nominate the specified `FilmName` as the film choice for the specified `DiscordUserID`.
+        If `DiscordUserID` is not a registered user, or `DiscordUserID` already has a nomination then throw an exception."""
+
+        try:
+            self.client.transact_write_items(
+                TransactItems=[
+                    {
+                        "Update": {
+                            "TableName": MEMBERS_TABLE,
+                            "Key": {"discord-user-id": {"S": DiscordUserID}},
+                            "ExpressionAttributeValues": {
+                                ":new_film_id": {"S": NewFilmID},
+                                ":null": {"NULL": True},
+                            },
+                            "ExpressionAttributeNames": {
+                                "#nominated": "nominated-film-id",
+                                "#discord-user-id": "discord-user-id",
+                            },
+                            "ConditionExpression": "attribute_exists(#discord-user-id) AND #nominated = :null",
+                            "UpdateExpression": "SET #nominated = :new_film_id",
+                        }
+                    },
+                    {
+                        "Put": {
+                            "TableName": NOMINATIONS_TABLE,
+                            "Item": {
+                                "film-id": {"S": NewFilmID},
+                                "imdb-film-id": {"NULL": True},
+                                "film-title": {"S": FilmName},
+                                "film-genre": {"NULL": True},
+                                "discord-user-id": {"S": DiscordUserID},
+                                "cast-votes": {"N": "0"},
+                                "attendance-votes": {"N": "0"},
+                                "date-nominated": {"S": DateTime.isoformat()},
+                                "date-watched": {"NULL": True},
+                            },
+                        }
+                    },
+                ]
+            )
+        except self.client.exceptions.TransactionCanceledException as e:
+            # Note that we can hit this if the user is not registered too, but all
+            # users in the Discord channel should be registered.  Ideally the
+            # `TransactionCanceledException` would return the existing value in the
+            # database (when setting) `ReturnValuesOnConditionCheckFailure`, but it
+            # only works in the Java SDK.
+            raise UserError(
+                "Unable to nominate a film as you have already nominated one"
+            )
 
     def cast_preference_vote(self, discord_user_id, imdb_film_id):
         # TODO
--- discord_handler/test_filmbot.py
+++ discord_handler/test_filmbot.py
@@ -1,9 +1,10 @@
 import unittest
 import boto3
-from botocore.exceptions import ClientError
 from moto import mock_dynamodb2
 from filmbot import FilmBot, MEMBERS_TABLE, NOMINATIONS_TABLE, VOTES_TABLE
 from datetime import datetime, timedelta
+from uuid import uuid1
+from UserError import UserError
 
 AWS_REGION = "eu-west-2"
 
@@ -49,7 +50,7 @@ class TestFilmBot(unittest.TestCase):
         )
 
         # Create our `FilmBot`
-        self.filmbot = FilmBot(self.dynamodb)
+        self.filmbot = FilmBot(self.dynamodb, self.dynamodb_client)
         pass
 
     def tearDown(self):
@@ -89,13 +90,75 @@ class TestFilmBot(unittest.TestCase):
         )
 
         # Check registering duplicate Discord IDs throw
-        self.assertRaises(
-            ClientError,
-            self.filmbot.register_user,
-            dummy_user_id,
-            time_registered + timedelta(days=1),
-        )
+        with self.assertRaises(UserError):
+            self.filmbot.register_user(
+                dummy_user_id, time_registered + timedelta(days=1)
+            )
         pass
+
+    def test_nominate_film(self):
+        dummy_user_id = "12345"
+        time_registered = datetime(2000, 12, 31, 23, 59, 59, 999999)
+        self.filmbot.register_user(dummy_user_id, time_registered)
+
+        film_id = str(uuid1())
+        film_name = "My Film"
+        nominated = datetime(2001, 1, 2, 3, 4, 5)
+
+        self.filmbot.nominate_film(
+            DiscordUserID=dummy_user_id,
+            FilmName=film_name,
+            NewFilmID=film_id,
+            DateTime=nominated,
+        )
+
+        members_table = self.dynamodb.Table(MEMBERS_TABLE)
+        members_record = members_table.get_item(Key={"discord-user-id": dummy_user_id})
+        self.assertEqual(
+            members_record["Item"],
+            {
+                "discord-user-id": dummy_user_id,
+                "date-joined": time_registered.isoformat(),
+                "nominated-film-id": film_id,
+                "vote-id": None,
+                "attendance-vote-id": None,
+            },
+        )
+
+        nominations_table = self.dynamodb.Table(NOMINATIONS_TABLE)
+        nominations_record = nominations_table.get_item(Key={"film-id": film_id})
+        self.assertEqual(
+            nominations_record["Item"],
+            {
+                "film-id": film_id,
+                "imdb-film-id": None,
+                "film-title": film_name,
+                "film-genre": None,
+                "discord-user-id": dummy_user_id,
+                "cast-votes": 0,
+                "attendance-votes": 0,
+                "date-nominated": nominated.isoformat(),
+                "date-watched": None,
+            },
+        )
+
+        # Check nominating fails when you already have a nomination
+        with self.assertRaises(UserError):
+            self.filmbot.nominate_film(
+                DiscordUserID=dummy_user_id,
+                FilmName="My New Film",
+                NewFilmID=str(uuid1()),
+                DateTime=nominated,
+            )
+
+        # Check nominating fails for a non-registered user
+        with self.assertRaises(UserError):
+            self.filmbot.nominate_film(
+                DiscordUserID="non-existent user",
+                FilmName="My New Film",
+                NewFilmID=str(uuid1()),
+                DateTime=nominated,
+            )
 
 
 if __name__ == "__main__":
